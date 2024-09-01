b21cc2cfbea8a959cf4cec308969569c8b1a10f0
parkpljec <park.pljec@gmail.com>
2024-09-01 21:41:23 +0900

    240901 1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props){
+  console.log('props', props.title)
+  return(
+    <div>
+      <header>
+        <h1><a href="/">{props.title}</a></h1>
+      </header>
+    </div>
+  )
+}
+
+function Article(props){
+return(
+  <article>
+  <h2>{props.title}</h2>
+  {props.body}  
+  </article>
+)
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="REACT"></Header>
+      <Header />
+      <Header />
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+      <Article title="Welcome"  body="Hello, Web"></Article>
+      <Article title="Hi"  body="Hello, React"></Article>
     </div>
   );
 }
-
 export default App;
